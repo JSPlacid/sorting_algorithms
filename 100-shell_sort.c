eaf19dcb9309330c5aef28eda7b3d2bde21648b5
Segun <ojoolusegun17@gmail.com>
2023-01-24 15:24:14 -0800

    shell sort

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -7,6 +7,7 @@
  * @a: The first integer to swap.
  * @b: The second integer to swap.
  */
+
 void swap_ints(int *a, int *b)
 {
 	int tmp;
@@ -20,9 +21,11 @@ void swap_ints(int *a, int *b)
  * shell_sort - Sort an array of integers in ascending
  *              order using the shell sort algorithm.
  * @array: An array of integers.
- * @size: The size of the array
+ * @size: The size of the array.
+ *
  * Description: Uses the Knuth interval sequence.
  */
+
 void shell_sort(int *array, size_t size)
 {
 	size_t gap, i, j;
