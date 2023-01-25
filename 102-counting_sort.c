3059caf94c7b5695e9bd175675cd8a169a86d0d9
Segun <ojoolusegun17@gmail.com>
2023-01-24 20:26:46 -0800

    counting sort fixed

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -21,7 +21,6 @@ int get_max(int *array, int size)
 }
 
 /**
-
  * counting_sort - Sort an array of integers in ascending order
  *                 using the counting sort algorithm.
  * @array: An array of integers.
